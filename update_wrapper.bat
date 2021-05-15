c81fac71884bd1031681c48dd8fc3598dfe11d06
Rooster Roosie <ohohlensroosterroosie@gmail.com>
2021-05-15 15:30:21 +0100

    Initial commit

---
+++ update_wrapper.bat
@@ -0,0 +1,69 @@
+:: Wrapper: Offline Updater
+:: Author: octanuary#6596 (narutofan420)
+:: License: MIT
+title Vyond Legacy [Initializing...]
+
+::::::::::::::::::::
+:: Initialization ::
+::::::::::::::::::::
+
+:: Stop commands from spamming stuff, cleans up the screen
+@echo off && cls
+
+:: Lets variables work or something idk im not a nerd
+SETLOCAL ENABLEDELAYEDEXPANSION
+
+:: Make sure we're starting in the correct folder, and that it worked (otherwise things would go horribly wrong)
+pushd "%~dp0"
+if !errorlevel! NEQ 0 goto error_location
+if not exist utilities ( goto error_location )
+if not exist wrapper ( goto error_location )
+if not exist server ( goto error_location )
+goto noerror_location
+:error_location
+echo Doesn't seem like this script is in a Vyond Legacy folder.
+pause && exit
+:noerror_location
+
+:::::::::::::::::::::::::
+:: Post-Initialization ::
+:::::::::::::::::::::::::
+
+title Vyond Legacy
+
+if not exist .git ( goto nogit )
+:yesgit
+echo Vyond Legacy Updater
+echo A project from VisualPlugin adapted by Benson and the Wrapper: Offline Team
+echo:
+echo Enter 1 to Vyond Legacy ^(⚠ It will remove any mods back them up before doing this ⚠)
+echo Enter 0 to close the updater
+goto wrapperidle
+:nogit
+echo You have not downloaded Vyond Legacy using the batch installer.
+echo Please download install_wrapper_offline.zip and run the batch file inside.
+pause & exit
+:wrapperidle
+echo:
+
+:::::::::::::
+:: Choices ::
+:::::::::::::
+
+set /p CHOICE=Choice:
+if "!choice!"=="0" goto exit
+if "!choice!"=="1" goto update
+echo Time to choose. && goto wrapperidle
+
+:update
+cls
+pushd "%~dp0"
+echo Pulling repository from GitHub...
+git pull
+cls
+echo Vyond Legacy has been updated^^!
+start "" "%~dp0"
+pause & exit
+
+:exit
+pause & exit
